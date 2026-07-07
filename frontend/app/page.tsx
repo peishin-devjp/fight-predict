4e0cc5c99633c2c8acb1091787379e8f2844d1fe
peishin-devjp <ozwald.so.cute@gmail.com>
2026-07-07 21:29:14 +0900

    Implement top page and create EventCard component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,55 @@
+import EventCard from "@/components/EventCard";
+
 export default function Home() {
   return (
     <div className="max-w-5xl mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-8">
-        格闘技勝敗予想サイト
+      <h1  className="text-3xl font-bold mb-8">
+        Fight Predict
       </h1>
+      <p className="mb-10 text-lg text-gray-600">
+        格闘技の勝敗予想を<br />
+        みんなで楽しもう
+      </p>
 
       <section className="mb-10">
         <h2 className="text-2xl font-semibold border-b pb-2 mb-4">
-          開催予定試合
+          注目試合
         </h2>
 
-        <div className="border rounded-lg p-4">
-          <p className="text-sm text-gray-500">UFC 320</p>
+        <div className="mt-4 border rounded-lg p-4">
+          <p className="text-base font-semibold text-gray-600">UFC 320</p>
 
           <h3 className="text-xl font-bold mt-2">
             アレックス・ペレイラ vs マゴメド・アンカラエフ
           </h3>
-
           <p className="mt-2">
             開催日：2026/7/20
           </p>
+          <button className="mt-3 rounded px-3 py-2 bg-black text-white">
+            勝敗を予想する
+          </button>
         </div>
+
+        <h2 className="mt-10 text-2xl font-semibold border-b pb-2 mb-4">
+          開催予定大会
+        </h2>
+
+        <EventCard
+          eventName="RIZIN 33"
+          mainCard="朝倉海 vs 佐々木ウルカ"
+          eventDate="2026/08/10"
+        />
+
+        <EventCard
+          eventName="DEEP 101"
+          mainCard="タイガ vs 火の鳥"
+          eventDate="2026/08/25"
+        />
+
+        <button className="mt-6 rounded bg-black px-6 py-3 text-white">
+          他大会を見る
+        </button>
+
       </section>
     </div>
   );
---
+++ frontend/components/EventCard.tsx
@@ -0,0 +1,29 @@
+type EventCardProps = {
+  eventName: string;
+  mainCard: string;
+  eventDate: string;
+};
+
+export default function EventCard({
+  eventName,
+  mainCard,
+  eventDate,
+}: EventCardProps) {
+  return (
+    <div className="border rounded-lg p-4 text-base mb-4">
+      <h3>{eventName}</h3>
+
+      <p className="font-bold mt-1">
+        {mainCard}
+      </p>
+
+      <p className="mt-1">
+        開催日：{eventDate}
+      </p>
+
+      <button className="mt-3 rounded px-3 py-2 bg-black text-white">
+        試合一覧を見る
+      </button>
+    </div>
+  );
+}
