38d2e151f905e97ebea56731ca047290f5b8102e
peishin-devjp <ozwald.so.cute@gmail.com>
2026-07-17 22:46:58 +0900

    Add events API endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,4 +6,14 @@ app.get("/", (req, res) => {
   res.send("Fight Predict API");
 });
 
+app.get("/events", (req, res) => {
+  res.json([
+    {
+      id: "ufc320",
+      name: "UFC 320",
+      date: "2026-09-20",
+    },
+  ]);
+});
+
 export default app;
